bba0123dc7a194a2e22463916aa9bf1d2ec2263f
Prince Owusu <32231909+prince272@users.noreply.github.com>
2022-07-13 18:50:11 +0000

    Added the unit of work pattern.

---
+++ src/CleanArchitecture.Core/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿namespace CleanArchitecture.Core
+{
+    public interface IUnitOfWork
+    {
+        void Add<TEntity>(IEnumerable<TEntity> entities) where TEntity : class, IEntity;
+
+        void Add<TEntity>(params TEntity[] entities) where TEntity : class, IEntity;
+
+        Task<TEntity?> FindAsync<TEntity>(long id) where TEntity : class, IEntity;
+
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class, IEntity;
+
+        void Remove<TEntity>(IEnumerable<TEntity> entities) where TEntity : class, IEntity;
+
+        void Remove<TEntity>(params TEntity[] entities) where TEntity : class, IEntity;
+
+        Task SaveChangesAsync();
+
+        void Update<TEntity>(IEnumerable<TEntity> entities) where TEntity : class, IEntity;
+
+        void Update<TEntity>(params TEntity[] entities) where TEntity : class, IEntity;
+    }
+}
