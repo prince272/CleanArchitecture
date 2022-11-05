b7317f2a739653893a55b77cfa77412fb1427da0
Prince Owusu <princeowusu.272@gmail.com>
2022-11-05 18:47:14 +0000

    Commit.

--- src/CleanArchitecture.Core/Utilities/Algorithm.cs
+++ src/CleanArchitecture.Core/Utilities/Algorithm.cs
@@ -34,13 +34,13 @@ namespace CleanArchitecture.Core.Utilities
 
         // URL Slugify algorithm in C#?
         // source: https://stackoverflow.com/questions/2920744/url-slugify-algorithm-in-c/2921135#2921135
-        public static string GenerateSlug(string text, string separator)
+        public static string GenerateSlug(string input, string separator)
         {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
 
             if (separator == null)
-                throw new ArgumentNullException(nameof(text));
+                throw new ArgumentNullException(nameof(input));
 
             static string RemoveDiacritics(string text)
             {
@@ -60,18 +60,18 @@ namespace CleanArchitecture.Core.Utilities
             }
 
             // remove all diacritics.
-            text = RemoveDiacritics(text);
+            input = RemoveDiacritics(input);
 
             // Remove everything that's not a letter, number, hyphen, dot, whitespace or underscore.
-            text = Regex.Replace(text, @"[^a-zA-Z0-9\-\.\s_]", string.Empty, RegexOptions.Compiled).Trim();
+            input = Regex.Replace(input, @"[^a-zA-Z0-9\-\.\s_]", string.Empty, RegexOptions.Compiled).Trim();
 
             // replace symbols with a hyphen.
-            text = Regex.Replace(text, @"[\-\.\s_]", separator, RegexOptions.Compiled);
+            input = Regex.Replace(input, @"[\-\.\s_]", separator, RegexOptions.Compiled);
 
             // replace double occurrences of hyphen.
-            text = Regex.Replace(text, @"(-){2,}", "$1", RegexOptions.Compiled).Trim('-');
+            input = Regex.Replace(input, @"(-){2,}", "$1", RegexOptions.Compiled).Trim('-');
 
-            return text;
+            return input;
         }
 
         public static string GenerateHash(string input)
@@ -110,6 +110,33 @@ namespace CleanArchitecture.Core.Utilities
                 sb.Append(hashBytes[i].ToString("X2"));
             }
             return sb.ToString();
+        }
+
+        public const string NATURAL_NUMERIC_CHARS = "123456789";
+        public const string WHOLE_NUMERIC_CHARS = "0123456789";
+        public const string LOWER_ALPHA_CHARS = "abcdefghijklmnopqrstuvwyxz";
+        public const string UPPER_ALPHA_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // How can I generate random alphanumeric strings?
+        // source: https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings
+        public static string GenerateText(int size, string characters)
+        {
+            var charArray = characters.ToCharArray();
+            byte[] data = new byte[4 * size];
+            using (var crypto = RandomNumberGenerator.Create())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                var rnd = BitConverter.ToUInt32(data, i * 4);
+                var idx = rnd % charArray.Length;
+
+                result.Append(charArray[idx]);
+            }
+
+            return result.ToString();
         }
 
         public static Guid CreateCryptographicallySecureGuid()
